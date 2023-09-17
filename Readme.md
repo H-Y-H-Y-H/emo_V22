7ac1cd0c2120ea9ee207a240b3c5c4cbb1a7a7ca
H-Y-H-Y-H <yuhang.hu@columbia.edu>
2023-09-17 15:28:04 -0400

    0917 realworld test ready from the desktop. Commit from the PC

--- Readme.md
+++ Readme.md
@@ -5,12 +5,12 @@ Use ```collect_data.py``` mode = 0 to collect robot random babbling data.
 # Target video landmarks collection
 Use ```collect_data.py``` mode = 1 to collect robot random babbling data.
 
--> 'data/emo_synced_lmks.npy'
+-> 'data/desktop/emo_synced_lmks.npy'
 
 # Landmarks to motor commands using Nearest Neighbor
 Use ```nn_baseline.py``` mode = 0 to find the index of the dataset that is closest to the target.
 
--> 'emo_nn_id.csv'
+-> 'data/nvidia/emo_nn_id.csv'
 
 
 
--- nn_baseline.py
+++ nn_baseline.py
@@ -9,10 +9,6 @@ if __name__ == "__main__":
 
     logger_id = []
 
-    time_s = time.time()
-    time0 = time.time()
-
-    frame_time = 1/30
 
     if mode == 0:
         data_path = 'data/desktop/'
@@ -49,44 +45,5 @@ if __name__ == "__main__":
 
             logger_id.append(best_nn_id)
         np.savetxt('data/nvidia/emo_nn_id.csv', np.asarray(logger_id), fmt='%i')
-
-
-    elif mode == 1:
-        from servo_m import *
-        dataset_pth = '../data0903/'
-        dataset_lmk = np.load(dataset_pth + 'm_lmks.npy')
-        dataset_cmd = np.loadtxt(dataset_pth + 'action.csv')
-
-        target_id = np.loadtxt('emo_purple_nn_id(smooth).csv')
-        cmds_list = []
-        for i in range(len(target_id)):
-            best_nn_id = int(target_id[i])
-            cmds = dataset_cmd[best_nn_id]
-            cmds_list.append(cmds)
-        np.savetxt('mimic_synced_cmds.csv', cmds_list)
-
-
-        #     move_all(cmds, interval=1)
-        #     time_left = time.time() - time0
-        #     if frame_time - time_left > 0:
-        #         time.sleep(frame_time - time_left)
-        #         print(time_left)
-        #     else:
-        #         print('out_of_time')
-        #     time0 = time.time()
-        # time_e = time.time()
-
-        # print(time_e - time_s)
-
-
-
-
-    # select_lmk = dataset_lmk[best_nn_id]
-    # plt.scatter(lmks[:,0],lmks[:,1],label='target')
-    # plt.scatter(select_lmk[:,0],select_lmk[:,1],label='select')
-    # plt.legend()
-
-    # plt.show()
-    # quit()
-
-    # time.sleep(0.04)
+        action_list = dataset_cmd[logger_id]
+        np.savetxt('data/nvidia/mimic_synced_cmds.csv', action_list)
